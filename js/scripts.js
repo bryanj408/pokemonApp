c9f5607f9018012bbbd703a6418c80e032f2d87a
bryanj408 <bryan.arndt52@gmail.com>
2022-11-01 14:20:10 -0700

    Added pokemonList array
    Cleaned up code in html

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,17 @@
-let myName = 'Bob';
-document.write(myName);
-myName = 'John Doe'
-document.write('myName');
+//Starting an array of pokemon
+let pokemonList = [
+  { name: 'Bulbasaur', 
+    height: 7, 
+    types: ['grass', 'poison'] 
+  },
+  { 
+    name: 'Charmander', 
+    height: 7, 
+    types: ['fire'] 
+  },
+  { 
+    name: 'Weedle', 
+    height: 5, 
+    types: ['bug,', 'poison']
+  }
+];
