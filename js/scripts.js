79deb1665522ac02821ff54cc25d35654b3c85b1
Bryan Arndt <38339891+bryanj408@users.noreply.github.com>
2022-11-04 15:10:53 -0700

    added for loop and conditional

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-//Starting an array of pokemon
+//Starting an array of pokemon (array with objects)
 let pokemonList = [
   { name: 'Bulbasaur', 
     height: 7, 
@@ -6,7 +6,7 @@ let pokemonList = [
   },
   { 
     name: 'Charmander', 
-    height: 7, 
+    height: 8, 
     types: ['fire'] 
   },
   { 
@@ -15,3 +15,44 @@ let pokemonList = [
     types: ['bug,', 'poison']
   }
 ];
+
+//for loop iterating through pokemonList array
+//
+for (i=0; i < pokemonList.length; i++) {
+  document.write(pokemonList[i].name +  ' (height:' + pokemonList[i].height + ') ' ); 
+
+  if (pokemonList[i].height > 7){
+    document.write(' - This one\'s a big guy! ');
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+//for loop
+// for (let i = 1; i <=100; i++){
+//   console.log(i);
+// }
+
+// let ages = [20, 30, 40, 50, 60];
+
+// for (i=0; i < ages.length; i++){
+//   console.log(ages[i] - 2);
+// } 
+
+//while loop
+// let i = 1;
+// while( i < 5){
+//   console.log(i);
+//   i++;
+// }
+
+
