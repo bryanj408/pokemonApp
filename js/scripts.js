b922647ee42fcf06bdc37b88f5d45b255be6eb34
Bryan Arndt <38339891+bryanj408@users.noreply.github.com>
2022-12-27 11:21:45 -0800

    added loadList() and loadDetails()
    modified add() to include typeof conditional. fixed button event listener to listen for event to fire showDetails()

--- js/scripts.js
+++ js/scripts.js
@@ -5,10 +5,6 @@ let pokemonRepository = (function () {
   let pokemonList = [];
   let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
-  function add(pokemon) {
-    pokemonList.push(pokemon);
-  }
-
   //function to create li/button/class for pokemon ui layout
   //which iterates through forEach() outside of iife
   function addListItem(pokemon) {
@@ -21,27 +17,61 @@ let pokemonRepository = (function () {
     list.appendChild(listItem);
 
     //logs pokemon details when button is clicked
-    button.addEventListener('click', function() {
-      console.log(pokemon);
+    button.addEventListener('click', function(event) {
+      showDetails(pokemon); 
     })
   }
 
-    //function that uses fetch and promise to load list of pokemon
-    function loadList() {
-      return fetch(apiUrl).then((response) => {
-        return response.json();
-      }).then((json) => {
-        json.results.forEach((item) => {
-          let pokemon = {
-            name: item.name,
-            detailsUrl: item.url,
-          };
-          add(pokemon);
-        });
-      }).catch((e) => {
-        console.log(e);
-      })
+  //function that uses fetch and promise to load list of pokemon
+  function loadList() {
+    return fetch(apiUrl).then((response) => {
+      return response.json();
+    }).then((json) => {
+      json.results.forEach((item) => {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url,
+        };
+        add(pokemon);
+      });
+    }).catch((e) => {
+      console.log(e);
+    })
+  }
+
+  //uses the detailsUrl property to load the detailed data for a given pokemon
+  function loadDetails(item) {
+    let url = item.detailsUrl;
+    return fetch(url).then((response) => {
+      return response.json();
+    }).then((details) => {
+      //come back to add 2nd sprite image
+      item.imageUrl = details.sprites.front_default;
+      item.height = details.height;
+      item.types = details.types;
+    }).catch((e) => {
+      console.error(e);
+    });
+  }
+
+  //validates typeof pokemon being added, prompts user to try again if failed
+  function add(pokemon) {
+    if (
+      typeof pokemon === 'object' &&
+      'name' in pokemon &&
+      'detailsUrl' in pokemon
+    ) {
+      pokemonList.push(pokemon);
+    }else{
+      console.log('Invalid pokemon. Please try again');
     }
+  }
+
+  function showDetails(pokemon) {
+    loadDetails(pokemon).then(() => {
+      console.log(pokemon);
+    });
+  }
 
   function getAll() {
     return pokemonList;
@@ -50,10 +80,11 @@ let pokemonRepository = (function () {
   return {
     add: add,
     loadList: loadList,
+    loadDetails: loadDetails,
+    showDetails: showDetails,
     addListItem: addListItem,
     getAll: getAll
   };
-
 })();
 
 //iterates through repository/loadlist then uses promise to load through repository/getAll
